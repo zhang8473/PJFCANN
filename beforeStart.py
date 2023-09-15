69c8f63a829f4461bb99d8fef2b7782eb50f73c3
zhang8473 <zhang8473@gmail.com>
2023-09-15 18:04:32 +0800

    1. speed up data process 2. resolve extra \n

--- beforeStart.py
+++ beforeStart.py
@@ -17,7 +17,7 @@ from itertools import islice
 from tqdm import tqdm
 from collections import Counter
 
-STOPWORDS = [':','：','、','\\','N','；',';','（','）','◆'
+STOPWORDS = [':','：','、','\\','N','；',';','（','）','◆','\n',
              '[',']','【','】','＋',',']
 
 
@@ -127,13 +127,13 @@ def prepro_each(args, data_path, start_ratio, stop_ratio, out_name):
     return
 
 
-def preprocess_Infer(args):
-    if not os.path.exists(args.train_test_dir):
-        os.makedirs(args.target_dir)
-
-    prepro_Infer_each(args, "data/step1_data/exp_morethan_50/data_train.json", "train")
-    prepro_Infer_each(args, "data/step1_data/exp_morethan_50/data_test.json", "test")
-    prepro_Infer_each(args, "data/step1_data/exp_morethan_50/data_dev.json", "dev")
+# def preprocess_Infer(args):
+#     if not os.path.exists(args.train_test_dir):
+#         os.makedirs(args.target_dir)
+#
+#     prepro_Infer_each(args, "data/step1_data/exp_morethan_50/data_train.json", "train")
+#     prepro_Infer_each(args, "data/step1_data/exp_morethan_50/data_test.json", "test")
+#     prepro_Infer_each(args, "data/step1_data/exp_morethan_50/data_dev.json", "dev")
 
 
 def prepro_Infer_each(args, data_path, out_name, exp_len = 50):
@@ -201,9 +201,9 @@ def preprocess_Graph(args):
     if not os.path.exists(args.train_test_dir):
         os.makedirs(args.train_test_dir)
 
+    prepro_Graph_each(args, "data/step1_data/exp_morethan_50_graph/data_dev.json", "dev")
     prepro_Graph_each(args, "data/step1_data/exp_morethan_50_graph/data_train.json", "train")
     prepro_Graph_each(args, "data/step1_data/exp_morethan_50_graph/data_test.json", "test")
-    prepro_Graph_each(args, "data/step1_data/exp_morethan_50_graph/data_dev.json", "dev")
 
 
 def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
@@ -220,10 +220,10 @@ def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
             source_data.append(json.loads(line))
 
     # Reading luqu.json
-    with open("data/step1_data/exp_morethan_50_graph/graph_user.json", 'r', encoding='utf8') as f:
+    with open("data/step1_data/exp_morethan_50_graph/graph_hired_user.json", 'r', encoding='utf8') as f:
         user_luqu_dict = json.load(f)
-    with open("data/step1_data/exp_morethan_50_graph/graph_jd.json", 'r', encoding='utf8') as f:
-        jd_luqu_dict = json.load(f)
+    with open("data/step1_data/exp_morethan_50_graph/graph_hired_jd.json", 'r', encoding='utf8') as f:
+        jd_recruit_dict = json.load(f)
 
     # Reading nothired.json
     # with open("data/step1_data/exp_morethan_50_graph/graph_nothired_user.json", 'r', encoding='utf8') as f:
@@ -241,13 +241,13 @@ def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
     user_id2num, user_num2id, jd_id2num, jd_num2id = load_index_table(args)
 
     # Generate and write
-    for i, content in enumerate(tqdm(source_data)):
+    for i, content in tqdm(enumerate(source_data)):
 
         jd_list = source_data[i]['job_posting']
         resume_list = source_data[i]['resume']
         job_id = source_data[i]['job_id']
         resume_id = source_data[i]['resume_id']
-        pair_id = source_data[i]['pair_id']
+        # pair_id = source_data[i]['pair_id']
         label = source_data[i]['label']
 
         # job posting
@@ -255,11 +255,7 @@ def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
         for job_req in jd_list:
             jd_content += job_req
         jd_line_list = list(word_tokenizer(jd_content))
-        jd_line = ""
-        for word in jd_line_list:
-            if word not in STOPWORDS:
-                jd_line += word + " "
-
+        jd_line = " ".join(word for word in jd_line_list if word not in STOPWORDS)
         # resume exp
         if isinstance(resume_list[0], list):
             resume_list = resume_list[0]
@@ -268,30 +264,23 @@ def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
         for exp in resume_list:
             cv_content += exp
         cv_line_list = list(word_tokenizer(cv_content))
-        cv_line = ""
-        for word in cv_line_list:
-            if word not in STOPWORDS:
-                cv_line += word + " "
-
+        cv_line = " ".join(word for word in cv_line_list if word not in STOPWORDS)
         # Graph cv (R-R)
-        R_R_list = []
-        if job_id in jd_luqu_dict:
-            user_luqu_list = jd_luqu_dict[job_id]
+        R_R_list = [user_id2num[resume_id]]
+        if job_id in jd_recruit_dict:
+            user_recruit_list = jd_recruit_dict[job_id]
         else:
-            print('jobID', job_id)
-            user_luqu_list = []
-        for user_item in user_luqu_list:
+            # print('jobID', job_id, 'does not exist.')
+            user_recruit_list = []
+        for user_item in user_recruit_list:
             if user_item != resume_id:
                 R_R_list.append(user_id2num[user_item])
                 # R_R_list.append(user_item)
         # R_R_line = resume_id + " "
-        R_R_line = str(user_id2num[resume_id]) + " "
-        for R in R_R_list:
-            R_R_line += str(R) + " "
-        R_R_line = R_R_line.strip()
+        R_R_line = " ".join([str(r_) for r_ in R_R_list])
 
         # Graph jd (J-J)
-        J_J_list = []
+        J_J_list = [jd_id2num[job_id]]
         if resume_id in user_luqu_dict:
             jd_luqu_list = user_luqu_dict[resume_id]
         else:
@@ -301,12 +290,7 @@ def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
             if jd_item != job_id:
                 # J_J_list.append(jd_item)
                 J_J_list.append(jd_id2num[jd_item])
-        # J_J_line = job_id + " "
-        J_J_line = str(jd_id2num[job_id]) + " "
-        for J in J_J_list:
-            J_J_line += str(J) + " "
-        J_J_line = J_J_line.strip()
-
+        J_J_line = " ".join([str(j_) for j_ in J_J_list])
         # write file
         if (len(cv_line.split(' ')) >= exp_len and len(jd_line.split(' ')) >= 15):
            #and len(R_R_list) and len(J_J_list)):
@@ -316,25 +300,31 @@ def prepro_Graph_each(args, data_path, out_name, exp_len=50, graph_num=5):
             jd_graph_file.write(J_J_line + '\n')
             cv_graph_file.write(R_R_line + '\n')
 
+    jd_write_file.close()
+    cv_write_file.close()
+    label_write_file.close()
+    jd_graph_file.close()
+    cv_graph_file.close()
+
 
 def index_table(config):
     user_id2num = {}
-    user_num2id = []
+    user_num2id = set()
     jd_id2num = {}
-    jd_num2id = []
-
+    jd_num2id = set()
+    import csv
     file_path = config.table_action
-    f =  open(file_path, 'r', encoding='utf8')
-    for line in islice(f, 1, None):
-        content_list = line.replace('\n','').split('\t')
+    f = open(file_path, 'r', encoding='utf8')
+    next(f)
+    csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+    for content_list in tqdm(csv_reader):
         user_id = content_list[0]
         jd_no = content_list[1]
-
-        if(user_id not in user_num2id):
-            user_num2id.append(user_id)
+        if user_id not in user_num2id:
+            user_num2id.add(user_id)
             user_id2num[user_id] = len(user_num2id) - 1
-        if(jd_no not in jd_num2id):
-            jd_num2id.append(jd_no)
+        if jd_no not in jd_num2id:
+            jd_num2id.add(jd_no)
             jd_id2num[jd_no] = len(jd_num2id) - 1
 
     print('User: ', len(user_num2id))
@@ -343,11 +333,11 @@ def index_table(config):
     f_save_uid2num = open(config.f_save_uid2num, 'w', encoding='utf8')
     json.dump(user_id2num, f_save_uid2num, ensure_ascii=False)
     f_save_unum2id = open(config.f_save_unum2id, 'w', encoding='utf8')
-    json.dump(user_num2id, f_save_unum2id, ensure_ascii=False)
+    json.dump(list(user_num2id), f_save_unum2id, ensure_ascii=False)
     f_save_jid2num = open(config.f_save_jid2num, 'w', encoding='utf8')
     json.dump(jd_id2num, f_save_jid2num, ensure_ascii=False)
     f_save_jnum2id = open(config.f_save_jnum2id, 'w', encoding='utf8')
-    json.dump(jd_num2id, f_save_jnum2id, ensure_ascii=False)
+    json.dump(list(jd_num2id), f_save_jnum2id, ensure_ascii=False)
 
     f_save_unum2id.close()
     f_save_uid2num.close()
@@ -357,11 +347,6 @@ def index_table(config):
 
 
 def load_index_table(config):
-    user_id2num = {}
-    user_num2id = []
-    jd_id2num = {}
-    jd_num2id = []
-
     f_save_uid2num = open(config.f_save_uid2num, 'r', encoding='utf8')
     user_id2num = json.load(f_save_uid2num)
     f_save_unum2id = open(config.f_save_unum2id, 'r', encoding='utf8')
@@ -378,16 +363,30 @@ def load_index_table(config):
     return user_id2num, user_num2id, jd_id2num, jd_num2id
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--table_action', default='Recruitment_round1_train_20190716\\table3_action')
-    parser.add_argument('--f_save_uid2num', default='Recruitment_round1_train_20190716\\uid2num.json')
-    parser.add_argument('--f_save_unum2id', default='Recruitment_round1_train_20190716\\unum2id.json')
-    parser.add_argument('--f_save_jid2num', default='Recruitment_round1_train_20190716\\jid2num.json')
-    parser.add_argument('--f_save_jnum2id', default='Recruitment_round1_train_20190716\\jnum2id.json')
-    config = parser.parse_args()
-    user_id2num, user_num2id, jd_id2num, jd_num2id = load_index_table(config)
-    return
+def split_train_test(input_, train_ratio, test_ratio, val_ratio):
+    assert train_ratio+test_ratio+val_ratio==1.0
+    import random
+    # Open the text file and read all lines
+    directory = os.path.dirname(input_)
+    with open(input_, 'r') as f:
+        lines = f.readlines()
+    # Shuffle the lines
+    random.shuffle(lines)
+    # Calculate the splits
+    train_size = int(len(lines) * train_ratio)
+    test_size = int(len(lines) * test_ratio)
+    val_size = int(len(lines) * val_ratio)
+    # Split the lines
+    train_lines = lines[:train_size]
+    test_lines = lines[train_size:train_size + test_size]
+    val_lines = lines[-val_size:]
+    # Write the splits to separate files
+    with open(os.path.join(directory, 'data_train.json'), 'w') as f:
+        f.writelines(train_lines)
+    with open(os.path.join(directory, 'data_test.json'), 'w') as f:
+        f.writelines(test_lines)
+    with open(os.path.join(directory, 'data_dev.json'), 'w') as f:
+        f.writelines(val_lines)
 
 
 import argparse
@@ -399,16 +398,11 @@ if __name__ == '__main__':
     parser.add_argument('--orignal_data_dir', default='Recruitment_round1_train_20190716')
     parser.add_argument('--tokenizer', default='jieba')
     parser.add_argument('--train_test_dir', default='data/train-test_data')
-    parser.add_argument('--table_action',
-                        default='Recruitment_round1_train_20190716\\table3_action')
-    parser.add_argument('--f_save_uid2num',
-                        default='Recruitment_round1_train_20190716\\uid2num.json')
-    parser.add_argument('--f_save_unum2id',
-                        default='Recruitment_round1_train_20190716\\unum2id.json')
-    parser.add_argument('--f_save_jid2num',
-                        default='Recruitment_round1_train_20190716\\jid2num.json')
-    parser.add_argument('--f_save_jnum2id',
-                        default='Recruitment_round1_train_20190716\\jnum2id.json')
+    parser.add_argument('--table_action', default='train-test_data\\table3_action')
+    parser.add_argument('--f_save_uid2num', default='Recruitment_round1_train_20190716\\uid2num.json')
+    parser.add_argument('--f_save_unum2id', default='Recruitment_round1_train_20190716\\unum2id.json')
+    parser.add_argument('--f_save_jid2num', default='Recruitment_round1_train_20190716\\jid2num.json')
+    parser.add_argument('--f_save_jnum2id', default='Recruitment_round1_train_20190716\\jnum2id.json')
     args = parser.parse_args()
 
     # Step 0. choose Processor according to the original data
@@ -433,6 +427,8 @@ if __name__ == '__main__':
     DataProcessor.dump_json('data/step1_data/exp_morethan_50_graph/graph_nothired_jd.json', mode='graph_nothired_jd')
     DataProcessor.dump_json('data/step1_data/exp_morethan_50_graph/graph_nothired_user.json', mode='graph_nothired_user')
 
-
     # 2. change data.json into train/test.json
+    split_train_test('data/step1_data/exp_morethan_50_graph/data.json', train_ratio=0.8, test_ratio=0.1, val_ratio=0.1)
+
+    index_table(args)
     preprocess_Graph(args)
